51e6194f61cce40c389c42c0ea0e66c4a1d391d3
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-10-30 10:38:42 -0300

    Update TrataCliente.java

--- Redes/src/sv/TrataCliente.java
+++ Redes/src/sv/TrataCliente.java
@@ -8,42 +8,57 @@ import java.io.ObjectOutputStream;
 import redes.Mensagem;
 
 public class TrataCliente implements Runnable {
-		
-		private Socket soquete_cliente;
-		private ObjectOutputStream saida;
-		private ObjectInputStream entrada;
-		private ArrayList<Mensagem> mensagens;
-		
-		public TrataCliente(Socket soquete_cliente, ArrayList<Mensagem> mensagens) throws Exception {
-			super();
-			this.soquete_cliente = soquete_cliente;
-			this.saida = new ObjectOutputStream(this.soquete_cliente.getOutputStream()); 
-			this.entrada = new ObjectInputStream(this.soquete_cliente.getInputStream());
-			this.mensagens = mensagens;
-		}
-		
-		public void enviar_mensagem(Object mensagem) throws Exception {
-			this.saida.writeObject(mensagem);
-		}
-		
-		public Object receber_mensagem() throws Exception {
-			return this.entrada.readObject();
-		}
-		
-		public void finalizar() throws IOException {
-			this.soquete_cliente.close();
-		}
-		
-		@Override
-		public void run() {
-			try {
-				Mensagem mensagem = (Mensagem)receber_mensagem();
-				System.out.println(mensagem.toString());
-				this.mensagens.add(mensagem);
-				enviar_mensagem(this.mensagens);
-				finalizar();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+
+    private Socket soquete_cliente;
+    private ObjectOutputStream saida;
+    private ObjectInputStream entrada;
+    private ArrayList<Mensagem> mensagens;
+
+    public TrataCliente(Socket soquete_cliente, ArrayList<Mensagem> mensagens) throws Exception {
+        super();
+        this.soquete_cliente = soquete_cliente;
+        this.saida = new ObjectOutputStream(this.soquete_cliente.getOutputStream());
+        this.entrada = new ObjectInputStream(this.soquete_cliente.getInputStream());
+        this.mensagens = mensagens;
+    }
+
+    public void enviar_mensagem(Object mensagem) throws Exception {
+        this.saida.writeObject(mensagem);
+    }
+
+    public Object receber_mensagem() throws Exception {
+        return this.entrada.readObject();
+    }
+
+    public void finalizar() throws IOException {
+        this.soquete_cliente.close();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Mensagem mensagem;
+            String comand[];
+            do {
+                mensagem = (Mensagem) receber_mensagem();
+                comand = mensagem.getTexto().split(";");
+
+                if (comand[0].equals("ENCERRAR")) {
+                    break;
+                } else if (comand[0].equals("ENVIAR")) {
+                    mensagem.setTexto(comand[1]);
+                    this.mensagens.add(mensagem);
+                } else if (comand[0].equals("LISTAR")) {
+                    if (comand[1].equals("CLIENTES")) {
+                        enviar_mensagem(Servidor.getClientes());
+                    } else if (comand[1].equals("MENSAGENS")) {
+                        enviar_mensagem(this.mensagens);
+                    }
+                }
+            } while (!mensagem.equals("ENCERRAR"));
+            finalizar();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

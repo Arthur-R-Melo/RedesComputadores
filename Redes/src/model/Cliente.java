a005ee3ecce0486e646c605ef517fcab6e635a4e
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-11-06 10:34:02 -0300

    Update Cliente.java

--- Redes/src/model/Cliente.java
+++ Redes/src/model/Cliente.java
@@ -4,10 +4,45 @@
  */
 package model;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 0068943
  */
-public class Cliente {
+public class Cliente implements Serializable{
+    private long id;
+    private String nome;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public Cliente(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + "," + this.nome;
+    }
+    
     
 }

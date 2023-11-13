d4700f7a2cea59d79c2c02974e1bd5db8e741824
Valter <103610549+valtersimao@users.noreply.github.com>
2023-11-13 10:46:02 -0300

    Update JanelaCliente.java

--- MensagensBanco/src/main/java/view/JanelaCliente.java
+++ MensagensBanco/src/main/java/view/JanelaCliente.java
@@ -206,8 +206,8 @@ public class JanelaCliente extends javax.swing.JFrame {
             String nome = this.jTextNome.getText();
             this.usuario = new Cliente("10.90.37.77", 15500, nome);
 
-            Mensagem msg = new Mensagem(nome, nome);
-            msg.setOperacao("ENTRAR");
+            Mensagem msg = new Mensagem(nome, "");
+            msg.setOperacao("ENTRAR;"+nome);
 
             this.usuario.enviar_mensagem(msg);
 

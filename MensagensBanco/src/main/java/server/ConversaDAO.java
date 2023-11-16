23566a46db6304097544d11169667a2b84dbeaa5
Arthur-R-Melo <arthurribeiromelo024@gmail.com>
2023-11-16 08:24:23 -0300

    Update ConversaDAO.java

--- MensagensBanco/src/main/java/server/ConversaDAO.java
+++ MensagensBanco/src/main/java/server/ConversaDAO.java
@@ -2,6 +2,11 @@
 package server;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import tools.FactoryPostgres;
 
 public class ConversaDAO {
@@ -12,6 +17,48 @@ public class ConversaDAO {
     }
     
     public int select(int id_remetente, int id_destinatario) {
-        String sql = "SELECT id FROM conversa WHERE cliente1 = ? OR ";
+        String sql = "SELECT id FROM conversa WHERE cliente1 = ? AND cliente2 = ? OR cliente1 = ? AND cliente2 = ?";
+        
+        try(PreparedStatement trans = c.prepareStatement(sql)) {
+            trans.setInt(1, id_remetente);
+            trans.setInt(4, id_remetente);
+            trans.setInt(2, id_destinatario);
+            trans.setInt(3, id_destinatario);
+            
+            ResultSet resultado = trans.executeQuery();
+            
+            if(resultado.next()) {
+                return resultado.getInt("id");
+            }else {
+                return insert(id_remetente, id_destinatario);
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            Logger.getLogger(ConversaDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return 0;
+        }
+    }
+    
+    public int insert(int id_remetente, int id_destinatario) {
+        String sql = "INSERT INTO conversa(cliente1,cliente2) VALUES (?,?) returning id";
+        
+        try(PreparedStatement trans = c.prepareStatement(sql)){
+            trans.setInt(1, id_remetente);
+            trans.setInt(2, id_destinatario);
+            
+            ResultSet result = trans.executeQuery();
+            
+            if(result.next()) {
+                return result.getInt("id");
+            }else {
+                System.err.println("Nao foi retornado nada");
+                return 0;
+            }
+            
+        } catch (SQLException ex) {
+            
+            System.out.println(ex.getMessage());
+            return 0;
+        }
     }
 }

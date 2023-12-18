a290920b602c79308f81dbe75f73262fb38eb681
Valter <103610549+valtersimao@users.noreply.github.com>
2023-12-18 09:50:39 -0300

    Update Cliente.java

--- MensagensBanco/src/main/java/model/Cliente.java
+++ MensagensBanco/src/main/java/model/Cliente.java
@@ -77,6 +77,7 @@ public class Cliente implements Serializable{
 
     @Override
     public String toString() {
-        return this.nome;
+        String on = this.online ? "ON" : "OFF";
+        return this.nome + "[" + on + "]";
     }
 }
